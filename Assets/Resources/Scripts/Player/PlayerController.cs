16593b90954eef634139ac7752b810fc7ba07e91
thefishlive <thefishlive@hotmail.com>
2016-04-16 09:10:28 +0100

    Add player movement and shape shifting

---
+++ Assets/Resources/Scripts/Player/PlayerController.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof (SpriteRenderer))]
+public class PlayerController : MonoBehaviour
+{
+    public enum Shape
+    {
+        Square,
+        Circle,
+        Diamond
+    }
+
+    [Header("Sensitivities")]
+    public float MovementSpeed = 1f;
+
+    [Header("Sprites")]
+    public Sprite CircleSprite = default(Sprite);
+    public Sprite SquareSprite = default(Sprite);
+    public Sprite DiamondSprite = default(Sprite);
+
+    private Shape m_shape;
+    private SpriteRenderer m_renderer;
+    private PlayerControls m_controls;
+
+	// Use this for initialization
+	void Start ()
+    {
+	    m_controls = new PlayerControls();
+
+	    m_renderer = GetComponent<SpriteRenderer>();
+        
+        m_shape = Shape.Square;
+        m_renderer.sprite = SquareSprite;
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+	{
+	    HandleInput();
+	}
+
+    private void HandleInput()
+    {
+        var movement = m_controls.Move.Value*MovementSpeed;
+        transform.position += new Vector3(movement.x, movement.y, 0);
+
+        if (m_controls.Shift.WasPressed)
+        {
+            Shift();
+        }
+    }
+
+    private void Shift()
+    {
+        switch (m_shape)
+        {
+            case Shape.Square:
+                m_shape = Shape.Circle;
+                m_renderer.sprite = CircleSprite;
+                break;
+            case Shape.Circle:
+                m_shape = Shape.Diamond;
+                m_renderer.sprite = DiamondSprite;
+                break;
+            case Shape.Diamond:
+                m_shape = Shape.Square;
+                m_renderer.sprite = SquareSprite;
+                break;
+        }
+    }
+}
---
+++ Assets/Resources/Scripts/Player/PlayerControls.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using InControl;
+using System;
+
+public class PlayerControls : PlayerActionSet
+{
+    public PlayerAction MoveUp { get; private set; }
+    public PlayerAction MoveDown { get; private set; }
+    public PlayerAction MoveLeft { get; private set; }
+    public PlayerAction MoveRight { get; private set; }
+    public PlayerTwoAxisAction Move { get; private set; }
+
+    public PlayerAction Shift { get; private set; }
+
+    public PlayerControls()
+    {
+        MoveUp = CreatePlayerAction("Move up");
+        MoveDown = CreatePlayerAction("Move down");
+        MoveLeft = CreatePlayerAction("Move left");
+        MoveRight = CreatePlayerAction("Move Right");
+
+        Move = CreateTwoAxisPlayerAction(MoveLeft, MoveRight, MoveDown, MoveUp);
+
+        Shift = CreatePlayerAction("Shapeshift");
+
+        SetupKeyboardDefaults();
+    }
+
+    private void SetupKeyboardDefaults()
+    {
+        MoveUp.AddDefaultBinding(Key.W);
+        MoveDown.AddDefaultBinding(Key.S);
+        MoveLeft.AddDefaultBinding(Key.A);
+        MoveRight.AddDefaultBinding(Key.D);
+
+        Shift.AddDefaultBinding(Key.Space);
+    }
+}
