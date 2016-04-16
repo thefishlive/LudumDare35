7f3a972fe41464a6e18e1b8b5c7df6c2859ba16e
thefishlive <thefishlive@hotmail.com>
2016-04-16 17:13:00 +0100

    Add walls to rooms

---
+++ Assets/Resources/Scripts/World/Room.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Room : MonoBehaviour
+{
+    [Header("Connections")]
+    public Room ConnectionLeft;
+    public Room ConnectionRight;
+    public Room ConnectionUp;
+    public Room ConnectionDown;
+
+    [Header("Room size")]
+    public int Width;
+    public int Height;
+
+    [Header("Prefabs")]
+    public GameObject WallPrefab;
+        
+    public void GenerateWalls()
+    {
+        // Top
+        var pos = transform.position + new Vector3(-(Width / 2f - 0.5f), Height / 2f - 0.5f, 0) ;
+
+        for (var i = 0; i < Width - 1; i++)
+        {
+            if (!(ConnectionUp != null && (i == (Width / 2) || i == (Width / 2) - 1)))
+            {
+                var wall = Instantiate(WallPrefab, pos, Quaternion.identity) as GameObject;
+                wall.transform.parent = transform;
+            }
+
+            pos += new Vector3(1, 0, 0);
+        }
+
+        // Right
+        pos = transform.position + new Vector3(Width / 2f - 0.5f, Height / 2f - 0.5f, 0);
+
+        for (var i = 0; i < Height - 1; i++)
+        {
+            if (!(ConnectionRight != null && (i == (Height / 2) || i == (Height / 2) - 1)))
+            {
+                var wall = Instantiate(WallPrefab, pos, Quaternion.identity) as GameObject;
+                wall.transform.parent = transform;
+            }
+
+            pos += new Vector3(0, -1, 0);
+        }
+
+        // Bottom
+        pos = transform.position + new Vector3(Width / 2f - 0.5f, -(Height / 2f - 0.5f), 0);
+
+        for (var i = 0; i < Width - 1; i++)
+        {
+            if (!(ConnectionDown != null && (i == (Width / 2) || i == (Width / 2) - 1)))
+            {
+                var wall = Instantiate(WallPrefab, pos, Quaternion.identity) as GameObject;
+                wall.transform.parent = transform;
+            }
+
+            pos += new Vector3(-1, 0, 0);
+        }
+
+        // Left
+        pos = transform.position + new Vector3(-(Width / 2f - 0.5f), -(Height / 2f - 0.5f), 0);
+
+        for (var i = 0; i < Height - 1; i++)
+        {
+            if (!(ConnectionLeft != null && (i == (Height / 2) || i == (Height / 2) - 1)))
+            {
+                var wall = Instantiate(WallPrefab, pos, Quaternion.identity) as GameObject;
+                wall.transform.parent = transform;
+            }
+
+            pos += new Vector3(0, 1, 0);
+        }
+
+    }
+}
--- Assets/Resources/Scripts/World/WorldGenerator.cs
+++ Assets/Resources/Scripts/World/WorldGenerator.cs
@@ -5,11 +5,17 @@ using Random = UnityEngine.Random;
 
 public class WorldGenerator : MonoBehaviour
 {
+    private const byte c_tunnelVertical = 0;
+    private const byte c_tunnelHorizontal = 1;
+
+    [Header("Random seed")]
+    public int RandomSeed = -1;
+
     [Header("Room Settings")]
     public int MinWidth = 5;
-    public int MaxWidth = 15;
+    public int MaxWidth = 10;
     public int MinHeight = 5;
-    public int MaxHeight = 15;
+    public int MaxHeight = 10;
     public int WorldSize = 5;
 
     [Header("Environment root")]
@@ -18,21 +24,21 @@ public class WorldGenerator : MonoBehaviour
     [Header("Prefabs")]
     public GameObject FloorPrefab;
     public GameObject PlayerPrefab;
+    public GameObject WallPrefab;
 
     private int m_roomId;
 
 	// Use this for initialization
 	void Start ()
 	{
-	    Random.seed = (int) DateTime.Now.Ticks;
-        var w = Random.Range(MinWidth, MaxWidth);
-        var h = Random.Range(MinHeight, MaxHeight);
+	    Random.seed = RandomSeed == -1 ? (int) DateTime.Now.Ticks : RandomSeed;
 
-        SpawnRoom(w, h, new Vector2(0, 0), 0);
+        SpawnRoom(RandomWidth(), RandomHeight(), new Vector2(0, 0), 0);
 
 	    foreach (Transform child in Env.transform)
 	    {
 	        child.GetComponent<BoxCollider2D>().enabled = false;
+            child.GetComponent<Room>().GenerateWalls();
 	    }
 
 	    SpawnPlayer(new Vector2(0, 0));
@@ -43,16 +49,21 @@ public class WorldGenerator : MonoBehaviour
         var player = Instantiate(PlayerPrefab, vector2, Quaternion.identity) as GameObject;
     }
 
-    private void SpawnRoom(int w, int h, Vector2 pos, int distance)
+    private Room SpawnRoom(int w, int h, Vector2 pos, int distance)
     {
+        // Spawn floor
         var obj = Instantiate(FloorPrefab, pos, Quaternion.identity) as GameObject;
         obj.transform.parent = Env;
         obj.transform.localScale = new Vector3(w, h, 1);
-        obj.name = "Floor " + m_roomId++;
+        obj.name = "Room " + m_roomId++;
+
+        var room = obj.GetComponent<Room>();
+        room.Width = w;
+        room.Height = h;
 
         if (distance > WorldSize)
         {
-            return;
+            return room;
         }
 
         int w2, h2;
@@ -61,82 +72,118 @@ public class WorldGenerator : MonoBehaviour
         // Right
         if (RandomBool())
         {
-            w2 = Random.Range(MinWidth, MaxWidth);
-            h2 = Random.Range(MinHeight, MaxHeight);
+            w2 = RandomWidth();
+            h2 = RandomHeight();
 
             pos2 = pos + new Vector2((w/2f) + 1 + (w2/2f), 0);
 
             if (Physics2D.BoxCastAll(pos2, new Vector2(w2, h2), 0f, Vector2.zero).Length == 0)
             {
-                SpawnRoom(w2, h2, pos2, distance + 1);
+                room.ConnectionRight = SpawnRoom(w2, h2, pos2, distance + 1);
+                SpawnTunnel(pos + new Vector2((w / 2f) + 0.5f, 0), c_tunnelHorizontal);
 
-                var tunnelPos = pos + new Vector2((w/2f) + 0.5f, 0);
-
-                var tunnel = Instantiate(FloorPrefab, tunnelPos, Quaternion.identity) as GameObject;
-                tunnel.transform.parent = Env;
+                if (room.ConnectionRight != null)
+                {
+                    room.ConnectionRight.ConnectionLeft = room;
+                }
             }
         }
 
         // Left
         if (RandomBool())
         {
-            w2 = Random.Range(MinWidth, MaxWidth);
-            h2 = Random.Range(MinHeight, MaxHeight);
+            w2 = RandomWidth();
+            h2 = RandomHeight();
 
             pos2 = pos + new Vector2(-((w / 2f) + 1 + (w2 / 2f)), 0);
 
             if (Physics2D.BoxCastAll(pos2, new Vector2(w2, h2), 0f, Vector2.zero).Length == 0)
             {
-                SpawnRoom(w2, h2, pos2, distance + 1);
+                room.ConnectionLeft = SpawnRoom(w2, h2, pos2, distance + 1);
+                SpawnTunnel(pos + new Vector2(-((w / 2f) + 0.5f), 0), c_tunnelHorizontal);
 
-                var tunnelPos = pos + new Vector2(-((w / 2f) + 0.5f), 0);
-
-                var tunnel = Instantiate(FloorPrefab, tunnelPos, Quaternion.identity) as GameObject;
-                tunnel.transform.parent = Env;
+                if (room.ConnectionLeft != null)
+                {
+                    room.ConnectionLeft.ConnectionRight = room;
+                }
             }
         }
 
         // Up
         if (RandomBool())
         {
-            w2 = Random.Range(MinWidth, MaxWidth);
-            h2 = Random.Range(MinHeight, MaxHeight);
+            w2 = RandomWidth();
+            h2 = RandomHeight();
 
             pos2 = pos + new Vector2(0, (h / 2f) + 1 + (h2 / 2f));
             
             if (Physics2D.BoxCastAll(pos2, new Vector2(w2, h2), 0f, Vector2.zero).Length == 0)
             {
-                SpawnRoom(w2, h2, pos2, distance + 1);
+                room.ConnectionUp = SpawnRoom(w2, h2, pos2, distance + 1);
+                SpawnTunnel(pos + new Vector2(0, (h / 2f) + 0.5f), c_tunnelVertical);
 
-                var tunnelPos = pos + new Vector2(0, (h / 2f) + 0.5f);
-
-                var tunnel = Instantiate(FloorPrefab, tunnelPos, Quaternion.identity) as GameObject;
-                tunnel.transform.parent = Env;
+                if (room.ConnectionUp != null)
+                {
+                    room.ConnectionUp.ConnectionDown = room;
+                }
             }
         }
 
         // Down
         if (RandomBool())
         {
-            w2 = Random.Range(MinWidth, MaxWidth);
-            h2 = Random.Range(MinHeight, MaxHeight);
+            w2 = RandomWidth();
+            h2 = RandomHeight();
 
             pos2 = pos + new Vector2(0, -((h / 2f) + 1 + (h2 / 2f)));
             
             if (Physics2D.BoxCastAll(pos2, new Vector2(w2, h2), 0f, Vector2.zero).Length == 0)
             {
-                SpawnRoom(w2, h2, pos2, distance + 1);
+                room.ConnectionDown = SpawnRoom(w2, h2, pos2, distance + 1);
+                SpawnTunnel(pos + new Vector2(0, -((h / 2f) + 0.5f)), c_tunnelVertical);
 
-                var tunnelPos = pos + new Vector2(0, -((h / 2f) + 0.5f));
-
-                var tunnel = Instantiate(FloorPrefab, tunnelPos, Quaternion.identity) as GameObject;
-                tunnel.transform.parent = Env;
+                if (room.ConnectionDown != null)
+                {
+                    room.ConnectionDown.ConnectionUp = room;
+                }
             }
         }
+
+        return room;
+    }
+
+    private void SpawnTunnel(Vector2 tunnelPos, byte direction)
+    {
+        var tunnel = Instantiate(FloorPrefab, tunnelPos, Quaternion.identity) as GameObject;
+        tunnel.name = "Tunnel" + m_roomId++;
+        tunnel.transform.parent = Env;
+        tunnel.transform.localScale = direction == c_tunnelHorizontal ? new Vector3(1, 2) : new Vector3(2, 1);
+
+        var wallPos1 = tunnelPos + (direction == c_tunnelHorizontal ? new Vector2(0, 1.5f) : new Vector2(1.5f, 0));
+        var tunnelWall1 = Instantiate(WallPrefab, wallPos1, Quaternion.identity) as GameObject;
+        tunnelWall1.name = "Tunnel Wall 1";
+        tunnelWall1.transform.parent = tunnel.transform;
+        tunnelWall1.transform.localScale = direction == c_tunnelHorizontal ? new Vector3(1, 0.5f, 1) : new Vector3(0.5f, 1, 1);
+
+        var wallPos2 = tunnelPos + (direction == c_tunnelHorizontal ? new Vector2(0, -1.5f) : new Vector2(-1.5f, 0));
+        var tunnelWall2 = Instantiate(WallPrefab, wallPos2, Quaternion.identity) as GameObject;
+        tunnelWall2.name = "Tunnel Wall 2";
+        tunnelWall2.transform.parent = tunnel.transform;
+        tunnelWall2.transform.localScale = direction == c_tunnelHorizontal ? new Vector3(1, 0.5f, 1) : new Vector3(0.5f, 1, 1);
     }
 
     private bool RandomBool()
     {
         return true;
+    }
+
+    private int RandomWidth()
+    {
+        return Random.Range(MinWidth, MaxWidth)*2;
+    }
+
+    private int RandomHeight()
+    {
+        return Random.Range(MinHeight, MaxHeight) * 2;
     }
 }
